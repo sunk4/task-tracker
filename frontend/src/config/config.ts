e2a0199064320ca5e9a58bf9dbb569b029e1d73d
Roman Trnka <trnka.roman13@gmail.com>
2024-10-12 12:53:14 +0200

    delete logs

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,7 +1,6 @@
 import { Configuration } from '../api';
 
 const basePath = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8088/api/v1';
-console.log('Base Path:', basePath); // Log to check the value
 
 export const configWithOutToken = new Configuration({
   basePath: basePath.replace(/\/+$/, ''),
