b6a6c689e9017b8eac375307cf2071c5ae4fc393
Roman Trnka <trnka.roman13@gmail.com>
2024-10-12 09:06:59 +0200

    prod

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,17 +1,17 @@
 INSERT INTO role (id, name, created_at)
 VALUES
-    ('de9e9c92-a8b4-4c38-bd98-ccc1f0e3fcc1', 'ROLE_ADMIN', NOW()),
-    ('d490df57-2bc9-43c6-8215-d8775758487d', 'ROLE_MANAGER', NOW()),
-    ('5ac27f6b-ffd3-4779-98e1-9d53865cb172', 'ROLE_USER', NOW())
+    ('de9e9c92-a8b4-4c38-bd98-ccc1f0e3fcc1', 'ROLE_ADMIN', CURRENT_TIMESTAMP),
+    ('d490df57-2bc9-43c6-8215-d8775758487d', 'ROLE_MANAGER', CURRENT_TIMESTAMP),
+    ('5ac27f6b-ffd3-4779-98e1-9d53865cb172', 'ROLE_USER', CURRENT_TIMESTAMP)
 ON CONFLICT (id) DO NOTHING;
 
 
 INSERT INTO users (id, account_locked, created_at, email, enabled, firstname, lastname, password, updated_at)
 VALUES
-    ('5e9cf932-c14e-4fcb-bcb1-9d60197b48df', false, NOW(), 'trnka.roman13@gmail.com', true, 'Roman', 'Trnka', '$2a$10$yCmRCqQ6yjF1cHk1Su9xX.17vqSKS9B/Wh.9yGjScj6BY7PwjZuIW', NULL),
-    ('b06e6acc-4c70-4856-8313-312933ce05d0', false, NOW(), 'admin@task-tracker.com', true, 'Admin', 'Trnka', '$2a$10$yCmRCqQ6yjF1cHk1Su9xX.17vqSKS9B/Wh.9yGjScj6BY7PwjZuIW', NULL),
-    ('fb3f9300-d19b-4007-b400-55d82fdde7e3', false, NOW(), 'manager@task-tracker.com', true, 'Manager', 'Trnka', '$2a$10$yCmRCqQ6yjF1cHk1Su9xX.17vqSKS9B/Wh.9yGjScj6BY7PwjZuIW', NULL),
-    ('e209eaad-8a1d-406f-8394-d66b2f382e03', false, NOW(), 'user@task-tracker.com', true, 'User', 'Trnka', '$2a$10$yCmRCqQ6yjF1cHk1Su9xX.17vqSKS9B/Wh.9yGjScj6BY7PwjZuIW', NULL)
+    ('5e9cf932-c14e-4fcb-bcb1-9d60197b48df', false, CURRENT_TIMESTAMP, 'trnka.roman13@gmail.com', true, 'Roman', 'Trnka', '$2a$10$yCmRCqQ6yjF1cHk1Su9xX.17vqSKS9B/Wh.9yGjScj6BY7PwjZuIW', NULL),
+    ('b06e6acc-4c70-4856-8313-312933ce05d0', false, CURRENT_TIMESTAMP, 'admin@task-tracker.com', true, 'Admin', 'Trnka', '$2a$10$yCmRCqQ6yjF1cHk1Su9xX.17vqSKS9B/Wh.9yGjScj6BY7PwjZuIW', NULL),
+    ('fb3f9300-d19b-4007-b400-55d82fdde7e3', false, CURRENT_TIMESTAMP, 'manager@task-tracker.com', true, 'Manager', 'Trnka', '$2a$10$yCmRCqQ6yjF1cHk1Su9xX.17vqSKS9B/Wh.9yGjScj6BY7PwjZuIW', NULL),
+    ('e209eaad-8a1d-406f-8394-d66b2f382e03', false, CURRENT_TIMESTAMP, 'user@task-tracker.com', true, 'User', 'Trnka', '$2a$10$yCmRCqQ6yjF1cHk1Su9xX.17vqSKS9B/Wh.9yGjScj6BY7PwjZuIW', NULL)
 ON CONFLICT (id) DO NOTHING;
 
 
